d6c8191677525495f081396fea9b04220faad206
Ngô Trường Sinh <sinhnt@bap.jp>
2020-10-06 21:36:15 +0700

    test cicd pipeline on push

--- src/main/kotlin/org/davidcampos/k8sjenkinsexample/GreetingController.kt
+++ src/main/kotlin/org/davidcampos/k8sjenkinsexample/GreetingController.kt
@@ -12,6 +12,6 @@ class GreetingController {
     @GetMapping("/greeting")
     fun greeting(@RequestParam(value = "name", defaultValue = "World!") name: String): Greeting {
         val envVar: String = System.getenv("EXAMPLE_VALUE") ?: "default_value"
-        return Greeting(counter.incrementAndGet(), "Hello, $name", envVar)
+        return Greeting(counter.incrementAndGet(), "Hello, $name!!!", envVar)
     }
 }
